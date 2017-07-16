2bf51321746b15ae90f0f0fe04638c3874cc613c
Anvesh Bandi <anvesh.bandi@in.pega.com>
2017-07-16 17:31:32 +0530

    export to excel

--- SCA-360/src/main/webapp/app/controllers/it/ituser.js
+++ SCA-360/src/main/webapp/app/controllers/it/ituser.js
@@ -164,5 +164,141 @@ angular
 							'read' : true
 						});
 					}
+					$scope.closecheck = function() {
+						if ($scope.ticket.status == 'Closed') {
+							var date = new Date();
+							$scope.ticket.enddate = date.getTime();
+							$scope.ticket.enddatestring = $filter('date')(
+									new Date($scope.ticket.enddate),
+									'yyyy-MM-dd HH:mm:ss');
+							$scope.close = true;
+
+						} else {
+							$scope.close = false;
+						}
+						if ($scope.ticket.status == 'Escalated') {
+							$scope.ticket.assignedto = 'SG - IT Admin';
+						} else if ($scope.ticket.status == 'In Progress') {
+							$scope.ticket.assignedto = 'IT Admin';
+						}
+
+					};
+
+					$scope.Workbook = function() {
+						if (!(this instanceof $scope.Workbook))
+							return new $scope.Workbook();
+						this.SheetNames = [];
+						this.Sheets = {};
+					}
+
+					$scope.sheet_from_array_of_arrays = function(data, opts) {
+						var ws = {};
+						var range = {
+							s : {
+								c : 10000000,
+								r : 10000000
+							},
+							e : {
+								c : 0,
+								r : 0
+							}
+						};
+						for (var R = 0; R != data.length; ++R) {
+							for (var C = 0; C != data[R].length; ++C) {
+								if (range.s.r > R)
+									range.s.r = R;
+								if (range.s.c > C)
+									range.s.c = C;
+								if (range.e.r < R)
+									range.e.r = R;
+								if (range.e.c < C)
+									range.e.c = C;
+								var cell = {
+									v : data[R][C]
+								};
+								if (cell.v == null)
+									continue;
+								var cell_ref = XLSX.utils.encode_cell({
+									c : C,
+									r : R
+								});
+
+								if (typeof cell.v === 'number')
+									cell.t = 'n';
+								else if (typeof cell.v === 'boolean')
+									cell.t = 'b';
+								else if (cell.v instanceof Date) {
+									cell.t = 'n';
+									cell.z = XLSX.SSF._table[14];
+									cell.v = datenum(cell.v);
+								} else
+									cell.t = 's';
+
+								ws[cell_ref] = cell;
+							}
+						}
+						if (range.s.c < 10000000)
+							ws['!ref'] = XLSX.utils.encode_range(range);
+						return ws;
+					}
+					$scope.s2ab = function(s) {
+						var buf = new ArrayBuffer(s.length);
+						var view = new Uint8Array(buf);
+						for (var i = 0; i != s.length; ++i)
+							view[i] = s.charCodeAt(i) & 0xFF;
+						return buf;
+					}
+
+					$scope.exportData = function() {
+						var A = [];
+						A.push(['','','','NewUserRequests ']);
+						A.push([' ']);
+						A.push([' ']);
+						
+						A.push([ 'Request ID','Employee Id','First name','Last Name','Assigned To','Email ID','Status' ]);
+						if ($scope.users.length > 0) {
+							for (var j = 0; j < $scope.users.length; j++) {
+								var B = [];
+
+								B.push($scope.users[j]['requestid']);
+								B.push($scope.users[j]['employeeid']);
+								B.push($scope.users[j]['firstname']);
+								B.push($scope.users[j]['lastname']);
+								B.push($scope.users[j]['emailid']);
+							
+								
+								
+								B.push($scope.users[j]['assignedto']);
+					
+								B.push($scope.users[j]['status']);
+								A.push(B);
+								if (j == $scope.users.length - 1) {
+									var data = A;
+									var ws_name = "users";
+
+									var wb = new $scope.Workbook(), ws = $scope
+											.sheet_from_array_of_arrays(data);
+
+									wb.SheetNames.push(ws_name);
+									wb.Sheets[ws_name] = ws;
+
+									/* write file */
+									var wopts = {
+										bookType : 'xlsx',
+										bookSST : false,
+										type : 'binary'
+									};
+
+									var wbout = XLSX.write(wb, wopts);
+
+									saveAs(new Blob([ $scope.s2ab(wbout) ], {
+										type : ""
+									}), "NewUserRequests.xlsx")
+								}
+							}
+						}
+
+					}
+
 
 				});
