f9bf43b4f9c96a0ce010d4ff7f53bff43132a95f
Anvesh Bandi <anvesh.bandi@in.pega.com>
2017-08-07 21:52:54 +0530

    fix menu

--- SCA-360/src/main/java/com/finsol/model/Menu.java
+++ SCA-360/src/main/java/com/finsol/model/Menu.java
@@ -2,7 +2,9 @@ package com.finsol.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -12,52 +14,32 @@ import org.hibernate.annotations.CascadeType;
 
 @Entity
 public class Menu {
-	
-	
-	
+
 	@Id
+	@GeneratedValue
+	private int id;
+	@Column
 	private String menuname;
-	
-	
-	
-	
-	@JoinColumn(name = "menuname")
+
+	@JoinColumn(name = "id")
 	@OneToMany
 	@Cascade({ CascadeType.ALL })
 	private List<SubMenu> submenulist;
-
-
-
 
 	public String getMenuname() {
 		return menuname;
 	}
 
-
-
-
 	public void setMenuname(String menuname) {
 		this.menuname = menuname;
 	}
-
-
-
 
 	public List<SubMenu> getSubmenulist() {
 		return submenulist;
 	}
 
-
-
-
 	public void setSubmenulist(List<SubMenu> submenulist) {
 		this.submenulist = submenulist;
 	}
-
-
-
-
-
-	
 
 }
